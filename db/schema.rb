822dad1c1c0a2ef9813f5025bde70fbda29923ad
Joe Villarba <socagambit@aol.com>
2016-02-26 01:27:53 -0800

    paperclip added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226090853) do
+ActiveRecord::Schema.define(version: 20160226092710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,12 @@ ActiveRecord::Schema.define(version: 20160226090853) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "certification"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
