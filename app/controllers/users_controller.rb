6e1e08f142d88af3551133d71a9563d0d0c97973
Joe Villarba <socagambit@aol.com>
2016-02-26 00:40:13 -0800

    user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,56 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
+    if current_user.id.to_s == params[:id]
+      @user = User.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      redirect_to edit_user_path(@user)
+    end
   end
 
   def destroy
+    if current_user.id.to_s == params[:id]
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :certification, :avatar)
+  end
+
 end
